e456d131cf51ee96ac2955de4a006d8c5ef4ca66
keerthan-poovaiah <keerthan11poovaiah@gmail.com>
2026-03-11 20:15:38 +0530

    DashBoard frontend

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,73 @@
-function dashboard() {
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
+function Dashboard() {
+
+  const [tickets, setTickets] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+
+    fetchTickets();
+
+  }, []);
+
+  const fetchTickets = async () => {
+
+    try {
+
+      const token = localStorage.getItem("token");
+
+      const res = await api.get("/tickets", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      setTickets(res.data.tickets);
+
+    } catch (error) {
+
+      alert("Failed to fetch tickets");
+
+    }
+
+  };
 
   return (
+
     <div>
+
       <h2>Dashboard</h2>
+
+      <button onClick={() => navigate("/create-ticket")}>
+        Create Ticket
+      </button>
+
+      <ul>
+
+        {tickets.map((ticket) => (
+
+          <li
+            key={ticket.id}
+            onClick={() => navigate(`/ticket/${ticket.id}`)}
+            style={{ cursor: "pointer", margin: "10px 0" }}
+          >
+
+            <strong>{ticket.title}</strong> - {ticket.status}
+
+          </li>
+
+        ))}
+
+      </ul>
+
     </div>
+
   );
 
 }
 
-export default dashboard;
+export default Dashboard;
