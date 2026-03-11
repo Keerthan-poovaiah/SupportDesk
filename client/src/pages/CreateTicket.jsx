78b84b71cbe585482a4f0b3832382f2b11dc14c3
keerthan-poovaiah <keerthan11poovaiah@gmail.com>
2026-03-11 20:19:51 +0530

    Frontend to create tickets

--- client/src/pages/CreateTicket.jsx
+++ client/src/pages/CreateTicket.jsx
@@ -1,11 +1,84 @@
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
 function CreateTicket() {
 
-  return (
-    <div>
-      <h2>Create Ticket Page</h2>
-    </div>
-  );
+  const navigate = useNavigate();
 
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("MEDIUM");
+
+  const handleSubmit = async () => {
+
+    try {
+
+      const token = localStorage.getItem("token");
+
+      await api.post(
+        "/tickets",
+        {
+          title,
+          description,
+          priority
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+
+      alert("Ticket created successfully");
+
+      navigate("/dashboard");
+
+    } catch (error) {
+
+      alert("Failed to create ticket");
+
+    }
+
+  };
+
+  return (
+
+    <div style={{ padding: "30px" }}>
+
+      <h2>Create Ticket</h2>
+
+      <input
+        placeholder="Title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <br /><br />
+
+      <textarea
+        placeholder="Description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <br /><br />
+
+      <select
+        onChange={(e) => setPriority(e.target.value)}
+      >
+        <option value="LOW">LOW</option>
+        <option value="MEDIUM">MEDIUM</option>
+        <option value="HIGH">HIGH</option>
+      </select>
+
+      <br /><br />
+
+      <button onClick={handleSubmit}>
+        Submit Ticket
+      </button>
+
+    </div>
+
+  );
 }
 
 export default CreateTicket;
