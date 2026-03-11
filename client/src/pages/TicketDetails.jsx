56801c8a3cf4d17042ccf5d3b8a4f41b9e0f833e
keerthan-poovaiah <keerthan11poovaiah@gmail.com>
2026-03-11 22:02:11 +0530

    Page with ticket details is added

--- client/src/pages/TicketDetails.jsx
+++ client/src/pages/TicketDetails.jsx
@@ -1,9 +1,112 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import { useParams } from "react-router-dom";
+
 function TicketDetails() {
 
+  const { id } = useParams();
+
+  const [ticket, setTicket] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+
+    fetchTicket();
+
+  }, []);
+
+  const fetchTicket = async () => {
+
+    try {
+
+      const token = localStorage.getItem("token");
+
+      const res = await api.get(`/tickets/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      setTicket(res.data.ticket);
+      setComments(res.data.comments);
+
+    } catch (error) {
+
+      alert("Failed to load ticket");
+
+    }
+
+  };
+
+  const addComment = async () => {
+
+    try {
+
+      const token = localStorage.getItem("token");
+
+      await api.post(
+        `/tickets/${id}/comments`,
+        { message },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+
+      setMessage("");
+
+      fetchTicket();
+
+    } catch (error) {
+
+      alert("Failed to add comment");
+
+    }
+
+  };
+
+  if (!ticket) return <p>Loading...</p>;
+
   return (
-    <div>
-      <h2>Ticket Details</h2>
+
+    <div style={{ padding: "30px" }}>
+
+      <h2>{ticket.title}</h2>
+
+      <p>{ticket.description}</p>
+
+      <p>Status: {ticket.status}</p>
+
+      <h3>Comments</h3>
+
+      <ul>
+
+        {comments.map((comment) => (
+
+          <li key={comment.created_at}>
+            <strong>{comment.name}</strong>: {comment.message}
+          </li>
+
+        ))}
+
+      </ul>
+
+      <h4>Add Comment</h4>
+
+      <input
+        placeholder="Write comment"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+
+      <button onClick={addComment}>
+        Submit
+      </button>
+
     </div>
+
   );
 
 }
